f8d991b9a0f234e7c441353d41b1c84b0807ac20
lemor.lee <lemor.lee@bfd10354-ac86-11dd-a0fe-71d3f493a0de>
2010-05-07 01:23:16 +0000

    增加子窗体；用于模型显示；

---
+++ trunk/EasyMidas/ChildForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using MidasGenModel.model;
+
+namespace EasyMidas
+{
+    public partial class ChildForm : Form
+    {
+        public Bmodel CurModel;
+        public ChildForm()
+        {
+            InitializeComponent();
+            CurModel = new Bmodel();
+        }
+
+        private void ChildForm_Paint(object sender, PaintEventArgs e)
+        {
+            label1.Text = "当前模型信息：节点" + CurModel.nodes.Count.ToString() +
+                "单元" + CurModel.elements.Count.ToString();
+        }
+    }
+}
